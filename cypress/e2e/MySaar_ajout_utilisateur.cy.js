d3865413a12d4aa8049b92bf243b353c18ff4f2a
victoria <moutcheuvictoria5@gmail.com>
2026-01-05 18:56:21 +0100

    ajout utilisateur

--- cypress/e2e/MySaar_ajout_utilisateur.cy.js
+++ cypress/e2e/MySaar_ajout_utilisateur.cy.js
@@ -4,8 +4,7 @@ Cypress.on('uncaught:exception', () => {
 
 describe('Offre d\'emploi', () => {
     beforeEach(() => {
-      cy.visit('http://10.0.102.67:4201/fr/auth/login');
-      cy.wait(1000)   
+      cy.visit('http://10.0.102.67:4201/fr/auth/login');  
       cy.get('input[placeholder="Votre identifiant"]') 
         .type('admin'); 
       cy.get('input[placeholder="Votre mot de passe')
@@ -16,20 +15,38 @@ describe('Offre d\'emploi', () => {
         .click();
     });
 
-    it('Ajout nominal d\'un utilisateur', () => {
-
-      cy.get('span.text-sm group-hover:text-red-600 transition-colors').click({force: true});
+    it('Ajout nominal d\'un utilisateur', () => { 
+      cy.wait(2000)
+      // cy.contains('span', /Utilisateurs/i).click({force: true});
+      cy.contains('a', /Utilisateurs/i).click({force: true});
       cy.contains('span', /Ajouter un utilisateur/i).click({force: true});
-      cy.get('input[formcontrolname="login"]').type('head');
-      cy.get('input[formcontrolname="password"]').type('saarsaar');
-      cy.get('input[formcontrolname="lastname"]').type('Kofane');
-      cy.get('input[formcontrolname="firstname"]').type('Tchamadeu');
-      cy.get('input[formcontrolname="mail"]').type('leocadiearistone3@gmail.com');
-      cy.get('input[formcontrolname="telephone"]').type('672962259');
-      cy.get('select[formcontrolname="usertype"]').select('ADMINISTRATEUR');
-      cy.contains('button', /Enregistrér/i).click();
-
+      cy.contains('p', 'Tous les champs de ce formulaire sont obligatoires.').should('be.visible');
+      cy.get('input[formcontrolname="login"]').type('THE fourth');
+      cy.get('input[formcontrolname="password"]').type('saarsaarsaraehrsaar');
+      cy.get('input[formcontrolname="lastName"]').type('Epassy');
+      cy.get('input[formcontrolname="firstName"]').type('Che');
+      cy.get('input[formcontrolname="email"]').type('leocadiearistone3@gmail.com');
+      cy.get('input[formcontrolname="telephone"]').type('692380589');
+      cy.get('select[formcontrolname="userType"]').select('ADMINISTRATEUR');
+   
+      cy.contains('button',  /Enregistrer/i).click()
+      cy.get('body', { timeout: 15000 }).then(($body) => {
+        if ($body.find('button:contains("OK")').length > 0) {
+            cy.contains('button', /OK/i).click({ force: true }, {timeout: 10000 });
+        } else {
+            cy.contains('button', 'Réessayer').click({ force: true });
+        }
     });
+}); 
+
+      // cy.contains('button', /Enregistrer/i).click();
+      // cy.contains('button', 'OK').click({force: true});
+   
+
+    // it('Suppression d\'un utilisateur', () => {
+      
+
+    // });
 
     
 });    
